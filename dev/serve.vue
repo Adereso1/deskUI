2b803e46d5bb6e7aef2701652965491875e055db
rholo <rahumada@adere.so>
2021-09-10 13:03:11 -0300

    feat(*): add initial config

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { DeskUiSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  DeskUiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <desk-ui-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as DeskUiSample } from './desk-ui-sample.vue';
